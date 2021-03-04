dabac496f45f67760d69157828e752610883671a
Leleat <atule@pm.me>
2021-03-04 20:13:20 +0100

    bugfix: DND area for top use workArea instead of panel; decrease DND preview area

--- tiling-assistant@leleat-on-github/extension.js
+++ tiling-assistant@leleat-on-github/extension.js
@@ -529,12 +529,11 @@ function onWindowMoving(window, grabStartPos, currTileGroup, screenRects, freeSc
 
 	const monitorNr = global.display.get_current_monitor();
 	const workArea = window.get_work_area_for_monitor(monitorNr);
-	const wRect = window.get_frame_rect();
 
-	const onTop = mouseY < main.panel.height + 25;
-	const onBottom = workArea.y + workArea.height - wRect.y < 40 || mouseY > workArea.y + workArea.height - 25;
-	const onLeft = mouseX <= workArea.x + 25;
-	const onRight = mouseX >= workArea.x + workArea.width - 25;
+	const onTop = mouseY < workArea.y + 10;
+	const onBottom = mouseY > workArea.y + workArea.height - 10;
+	const onLeft = mouseX < workArea.x + 10;
+	const onRight = mouseX > workArea.x + workArea.width - 10;
 
 	const tileTopLeftQuarter = onTop && onLeft;
 	const tileTopRightQuarter = onTop && onRight;
