b07405bb0471b4426573c9c7ef68b049da596141
Leleat <atule@pm.me>
2021-04-20 15:33:57 +0200

    tile editing mode: [u]->[r] to restore window size; cycle through window halve states instead of dedicated bindings of [h]/[v] because it's easier to remember

--- tiling-assistant@leleat-on-github/tilingEditingMode.js
+++ tiling-assistant@leleat-on-github/tilingEditingMode.js
@@ -88,7 +88,6 @@ var TileEditor = GObject.registerClass(class TilingEditingMode extends St.Widget
 		const keySym = keyEvent.keyval;
 		const modState = keyEvent.modifier_state;
 		const isCtrlPressed = modState & Clutter.ModifierType.CONTROL_MASK;
-		const isAltPressed = modState & Clutter.ModifierType.MOD1_MASK;
 		const isShiftPressed = modState & Clutter.ModifierType.SHIFT_MASK;
 		const isSuperPressed = modState & Clutter.ModifierType.MOD4_MASK;
 
@@ -105,51 +104,44 @@ var TileEditor = GObject.registerClass(class TilingEditingMode extends St.Widget
 			Util.tileWindow(window, tileRect, false);
 			this.select(window.tiledRect, window);
 
-		// [H]orizontally split tiled window
-		} else if ((keySym === Clutter.KEY_h || keySym === Clutter.KEY_H) && (isShiftPressed || isAltPressed)) {
+		// [C]ycle through halves of the available space of the window
+		} else if ((keySym === Clutter.KEY_c || keySym === Clutter.KEY_C)) {
 			const window = this._primaryIndicator.window;
 			if (!window)
 				return;
 
-			if (isShiftPressed) {
-				Util.tileWindow(window, new Meta.Rectangle({
-					x: window.tiledRect.x,
-					y: window.tiledRect.y,
-					width: Math.ceil(window.tiledRect.width / 2),
-					height: window.tiledRect.height
-				}), false);
-			} else {
-				Util.tileWindow(window, new Meta.Rectangle({
-					x: window.tiledRect.x + Math.floor(window.tiledRect.width / 2),
-					y: window.tiledRect.y,
-					width: Math.ceil(window.tiledRect.width / 2),
-					height: window.tiledRect.height
-				}), false);
-			}
-			this.select(window.tiledRect, window);
+			const getCenterPoint = rect => {return {x: rect.x + rect.width / 2, y: rect.y + rect.height / 2}};
+			const fullRect = Util.getBestFitTiledRect(window, this._topTileGroup);
+			const rects = [new Meta.Rectangle({
+				x: fullRect.x,
+				y: fullRect.y,
+				width: Math.ceil(fullRect.width / 2),
+				height: fullRect.height
+			}), new Meta.Rectangle({
+				x: fullRect.x,
+				y: fullRect.y,
+				width: fullRect.width,
+				height: Math.ceil(fullRect.height / 2)
+			}), new Meta.Rectangle({
+				x: fullRect.x + Math.floor(fullRect.width / 2),
+				y: fullRect.y,
+				width: Math.ceil(fullRect.width / 2),
+				height: fullRect.height
+			}), new Meta.Rectangle({
+				x: fullRect.x,
+				y: fullRect.y + Math.floor(fullRect.height / 2),
+				width: fullRect.width,
+				height: Math.ceil(fullRect.height / 2)
+			})];
 
-		// [V]ertically split tiled window
-		} else if ((keySym === Clutter.KEY_v || keySym === Clutter.KEY_V) && (isShiftPressed || isAltPressed)) {
-			const window = this._primaryIndicator.window;
-			if (!window)
-				return;
+			const currIdx = rects.findIndex(r => r.equal(window.tiledRect));
+			const newIndex = ((currIdx === -1 ? rects.reduce((closestRectIdx, r, idx) => {
+				return Util.distBetween2Points(getCenterPoint(rects[closestRectIdx]), getCenterPoint(window.tiledRect))
+						< Util.distBetween2Points(getCenterPoint(r), getCenterPoint(window.tiledRect)) ? closestRectIdx : idx;
+			}, 0) : currIdx) + 1) % 4;
 
-			if (isShiftPressed) {
-				Util.tileWindow(window, new Meta.Rectangle({
-					x: window.tiledRect.x,
-					y: window.tiledRect.y,
-					width: window.tiledRect.width,
-					height: Math.ceil(window.tiledRect.height / 2)
-				}), false);
-			} else {
-				Util.tileWindow(window, new Meta.Rectangle({
-					x: window.tiledRect.x,
-					y: window.tiledRect.y + Math.floor(window.tiledRect.height / 2),
-					width: window.tiledRect.width,
-					height: Math.ceil(window.tiledRect.height / 2)
-				}), false);
-			}
-			this.select(window.tiledRect, window);
+			Util.tileWindow(window, rects[newIndex], false);
+			this.select(rects[newIndex], window);
 
 		// [Q]uit a window
 		} else if (keySym === Clutter.KEY_q || keySym === Clutter.KEY_Q) {
@@ -162,17 +154,23 @@ var TileEditor = GObject.registerClass(class TilingEditingMode extends St.Widget
 			const newWindow = this._topTileGroup[0];
 			newWindow ? this.select(newWindow.tiledRect, newWindow) : this.close();
 
-		// [U]ntile a window
-		} else if (keySym === Clutter.KEY_u || keySym === Clutter.KEY_U) {
+		// [R]estore window size
+		} else if (keySym === Clutter.KEY_r || keySym === Clutter.KEY_R) {
 			const window = this._primaryIndicator.window;
 			if (!window)
 				return;
 
+			const ogRect = window.tiledRect.copy();
 			this._topTileGroup.splice(this._topTileGroup.indexOf(window), 1);
 			Util.restoreWindowSize(window, true);
+			if (!this._topTileGroup.length) {
+				this.close();
+				return;
+			}
+
 			this._topTileGroup.forEach(w => w.raise());
-			const newWindow = this._topTileGroup[0];
-			newWindow ? this.select(newWindow.tiledRect, newWindow) : this.close();
+			Util.getOpenWindows().find(w => this._topTileGroup.includes(w)).activate(global.get_current_time());
+			this.select(ogRect, null);
 
 		// [Esc]ape tile editing mode
 		} else if (keySym === Clutter.KEY_Escape) {
@@ -207,7 +205,7 @@ var TileEditor = GObject.registerClass(class TilingEditingMode extends St.Widget
 				});
 			}
 
-		// switch selection (WASD, hjkl and arrow keys)
+		// [Direction] (WASD, hjkl or arrow keys)
 		} else if (Util.eventIsDirection(keySym, Meta.MotionDirection.UP)) {
 			isSuperPressed ? this._resize(MainExtension.TILING.TOP, isShiftPressed)
 					: this._selectTowards(MainExtension.TILING.TOP, isCtrlPressed);
@@ -241,26 +239,31 @@ var TileEditor = GObject.registerClass(class TilingEditingMode extends St.Widget
 
 		const resizedRect = window.tiledRect.copy();
 		const workArea = window.get_work_area_current_monitor();
-		let resizeAmount = 100;
-		// limit resizeAmount when trying to extend outside of the current screen
+		let resizeStep = 100;
+		// limit resizeStep when trying to extend outside of the current screen
 		if (direction === MainExtension.TILING.TOP && isShiftPressed)
-			resizeAmount = Math.min(resizeAmount, resizedRect.y - workArea.y);
+			resizeStep = Math.min(resizeStep, resizedRect.y - workArea.y);
 		else if (direction === MainExtension.TILING.BOTTOM && !isShiftPressed)
-			resizeAmount = Math.min(resizeAmount, workArea.y + workArea.height - (resizedRect.y + resizedRect.height));
+			resizeStep = Math.min(resizeStep, workArea.y + workArea.height - (resizedRect.y + resizedRect.height));
 		else if (direction === MainExtension.TILING.LEFT && isShiftPressed)
-			resizeAmount = Math.min(resizeAmount, resizedRect.x - workArea.x);
+			resizeStep = Math.min(resizeStep, resizedRect.x - workArea.x);
 		else if (direction === MainExtension.TILING.RIGHT && !isShiftPressed)
-			resizeAmount = Math.min(resizeAmount, workArea.x + workArea.width - (resizedRect.x + resizedRect.width));
+			resizeStep = Math.min(resizeStep, workArea.x + workArea.width - (resizedRect.x + resizedRect.width));
+
+		if (!resizeStep) {
+			main.notify("Tiling Assistant", "Can't resize into that direction. Super + Directions resizes on the S and E side. Super + Shift + Directions on the N and W side.");
+			return;
+		}
 
 		const isVertical = direction === MainExtension.TILING.TOP || direction === MainExtension.TILING.BOTTOM;
 		const changeDir = ((direction === MainExtension.TILING.BOTTOM || direction === MainExtension.TILING.RIGHT) ? 1 : -1)
 				* (isShiftPressed ? -1 : 1);
 		const getResizedRect = function(rect, dimensionChangeOnly, dir) {
 			return new Meta.Rectangle({
-				x: rect.x + (dimensionChangeOnly || isVertical ? 0 : resizeAmount * -dir),
-				y: rect.y + (!dimensionChangeOnly && isVertical ? resizeAmount * -dir : 0),
-				width: rect.width + (isVertical ? 0 : resizeAmount * dir),
-				height: rect.height + (isVertical ? resizeAmount * dir : 0),
+				x: rect.x + (dimensionChangeOnly || isVertical ? 0 : resizeStep * -dir),
+				y: rect.y + (!dimensionChangeOnly && isVertical ? resizeStep * -dir : 0),
+				width: rect.width + (isVertical ? 0 : resizeStep * dir),
+				height: rect.height + (isVertical ? resizeStep * dir : 0),
 			});
 		};
 		const resizeSide = function(rect1, rect2, opposite) {
