10d64cdabcafbd289d5ec9b45bb3d0ef20050bdd
Leleat <atule@pm.me>
2024-05-10 15:38:32 +0200

    Settings: Integrate SettingsOverrider functionality
    ... quoting commit a0ecbd51bdebc70fae60621017f2b6fb6a516c62:
    
    The extension is overriding the user settings by writing on them, sadly this
    implies various issues because even if we reset them on extension unloading,
    a shell crash or mis-behavior could always lead to affect user settings.
    
    Tiling assistant replaces the user settings when running, but not to loose the
    user settings, we need to save them around so that when the extension gets
    unloaded they can be restored.
    
    So add a SettingsOverrider class that:
     - Replaces the user settings with the extensions one
     - Remembers what has been changed and saves it locally and in our settings
     - When the extension is unloaded the settings are restored or reset
    
    Now, in case gnome-shell is stopped or the extension has crashed for some
    reason, once the extension is reloaded we check if previous settings were
    saved and in case they are used to restore the user settings on destruction.

--- tiling-assistant@leleat-on-github/src/extension/settings.js
+++ tiling-assistant@leleat-on-github/src/extension/settings.js
@@ -1,3 +1,4 @@
+import { Gio, GLib } from '../dependencies/gi.js';
 import { Extension } from '../dependencies/shell.js';
 
 /** @type {Settings} */
@@ -20,11 +21,110 @@ function disable() {
  * and overrides.
  */
 class Settings {
+    /**
+     * @type {boolean} determines if the settings overrides have been properly
+     * reverted via the extensions `disable` the last time the extension was
+     * enabled. There are circumstances, when an extension's `disable` method
+     * isn't called e. g. when the system shuts down or when gnome-shell crashes.
+     */
+    _didntRevertPreviously;
     /** @type {Gio.Settings} */
     _gioObject = Extension.lookupByURL(import.meta.url).getSettings();
+    /**
+     * @type {Map<string, Map<string, GLib.Variant>>} saves the native settings
+     * that have been overridden in the extensions `enable` call. The key is the
+     * schema_id of the overridden Gio.Settings. The value is a Map of the
+     * overridden key and the settings old value.
+     */
+    _runtimeOverriddes = new Map();
+
+    constructor() {
+        this._didntRevertPreviously =
+            this._gioObject.get_user_value('overridden-settings') !== null;
+    }
 
     destroy() {
+        this._clearOverriddenSettings();
+
         this._gioObject = null;
+    }
+
+    /**
+     * Overrides GNOME's native settings and restores them on `disable()`.
+     *
+     * @param {Gio.Settings} settings
+     * @param {string} key
+     * @param {GLib.Variant} newValue
+     */
+    override(settings, key, newValue) {
+        const schemaId = settings.schema_id;
+        const userValue = settings.get_user_value(key);
+        const oldSettingsMap = this._runtimeOverriddes.get(schemaId);
+
+        if (oldSettingsMap)
+            oldSettingsMap.set(key, userValue);
+        else
+            this._runtimeOverriddes.set(schemaId, new Map([[key, userValue]]));
+
+        this._updateBackupOverrides(schemaId, key, userValue);
+        settings.set_value(key, newValue);
+    }
+
+    _clearOverriddenSettings() {
+        if (this._didntRevertPreviously) {
+            const previouslySavedSettings = this._gioObject
+                .get_value('overridden-settings')
+                .unpack();
+
+            Object.entries(previouslySavedSettings).forEach(([path, value]) => {
+                const splits = path.split('.');
+                const key = splits.at(-1);
+                const schemaId = splits.slice(0, -1).join('.');
+                const gobject = new Gio.Settings({ schema_id: schemaId });
+                const variant = value.get_variant();
+
+                if (
+                    variant.equal(
+                        GLib.Variant.new_maybe(new GLib.VariantType('b'), null)
+                    )
+                )
+                    gobject.reset(key);
+                else
+                    gobject.set_value(key, variant);
+            });
+        } else {
+            this._runtimeOverriddes.forEach((overrides, schemaId) => {
+                const gobject = new Gio.Settings({ schema_id: schemaId });
+
+                overrides.forEach((value, key) => {
+                    if (value)
+                        gobject.set_value(key, value);
+                    else
+                        gobject.reset(key);
+                });
+            });
+        }
+
+        this._gioObject.reset('overridden-settings');
+        this._runtimeOverriddes.clear();
+    }
+
+    _updateBackupOverrides(schemaId, key, newValue) {
+        if (this._didntRevertPreviously)
+            return;
+
+        const savedSettings = this._gioObject
+            .get_value('overridden-settings')
+            .deepUnpack();
+        const prefKey = `${schemaId}.${key}`;
+
+        savedSettings[prefKey] =
+            newValue ?? GLib.Variant.new_maybe(new GLib.VariantType('b'), null);
+
+        this._gioObject.set_value(
+            'overridden-settings',
+            new GLib.Variant('a{sv}', savedSettings)
+        );
     }
 
     /***************************************************************************
