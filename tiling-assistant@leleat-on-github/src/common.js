Leleat/Tiling-Assistant

commit 230559b62d090f78f9f5c7dd4ebca31b213bd925

Avoid calling Meta.is_wayland_compositor() if it doesn't exist
It's been removed in Mutter 50.alpha

tiling-assistant@leleat-on-github/src/common.js
@@ -3,6 +3,8 @@
  * *and* prefs files
  */
 
+import { Meta } from './dependencies/gi.js';
+
 /**
  * A Singleton providing access to the settings.
  */
@@ -321,3 +323,8 @@ var LayoutItem = class LayoutItem {
         this.loopType = null;
     }
 };
+
+export function is_wayland_compositor() {
+    return Meta.is_wayland_compositor === undefined ||
+           Meta.is_wayland_compositor();
+}

tiling-assistant@leleat-on-github/src/extension/moveHandler.js
@@ -2,7 +2,7 @@ import { Clutter, GLib, GObject, Gio, Meta, Mtk } from '../dependencies/gi.js';
 import { Main, WindowManager } from '../dependencies/shell.js';
 import { WINDOW_ANIMATION_TIME } from '../dependencies/unexported/windowManager.js';
 
-import { Orientation, MoveModes, Settings } from '../common.js';
+import { Orientation, MoveModes, Settings, is_wayland_compositor } from '../common.js';
 import { Rect, Util } from './utility.js';
 import { TilingWindowManager as Twm } from './tilingWindowManager.js';
 
@@ -319,7 +319,7 @@ export default class TilingMoveHandler {
         const altL = Clutter.ModifierType.MOD1_MASK;
         const altGr = Clutter.ModifierType.MOD5_MASK;
         const meta = Clutter.ModifierType.MOD4_MASK;
-        const rmb = Meta.is_wayland_compositor()
+        const rmb = is_wayland_compositor()
             ? Clutter.ModifierType.BUTTON2_MASK
             : Clutter.ModifierType.BUTTON3_MASK;
         const pressed = [ // idxs come from settings

tiling-assistant@leleat-on-github/src/extension/tilingPopup.js
@@ -1,8 +1,8 @@
-import { Clutter, GObject, Meta, St } from '../dependencies/gi.js';
+import { Clutter, GObject, St } from '../dependencies/gi.js';
 import { Main } from '../dependencies/shell.js';
 import * as SwitcherPopup from '../dependencies/unexported/switcherPopup.js';
 
-import { Direction, Orientation } from '../common.js';
+import { Direction, Orientation, is_wayland_compositor } from '../common.js';
 import { Util } from './utility.js';
 import { TilingWindowManager as Twm } from './tilingWindowManager.js';
 import * as AltTab from './altTab.js';
@@ -52,7 +52,7 @@ export const TilingSwitcherPopup = GObject.registerClass({
 
         // Destroy popup when touching outside of popup
         this.connect('touch-event', () => {
-            if (Meta.is_wayland_compositor())
+            if (is_wayland_compositor())
                 this.fadeAndDestroy();
 
             return Clutter.EVENT_PROPAGATE;

commit 8e0e68d48bc9c21f04ed68cf5d11a03c8f363f51

fix: move Meta.is_wayland_compositor from common.js to utility.js
230559 created a `is_wayland_compositor` check in common.js since mutter
removed it in 50. This broke the prefs window since the file imported
gi://Meta, which only exists in the Shell process but common.js is used
for both the (shell) extension and prefs.

So move the function to utility.js, which is intended only for shell
usage.

Fixes: 230559b62d090f78f9f5c7dd4ebca31b213bd925

tiling-assistant@leleat-on-github/src/common.js
@@ -3,8 +3,6 @@
  * *and* prefs files
  */
 
-import { Meta } from './dependencies/gi.js';
-
 /**
  * A Singleton providing access to the settings.
  */
@@ -323,8 +321,3 @@ var LayoutItem = class LayoutItem {
         this.loopType = null;
     }
 };
-
-export function is_wayland_compositor() {
-    return Meta.is_wayland_compositor === undefined ||
-           Meta.is_wayland_compositor();
-}

tiling-assistant@leleat-on-github/src/extension/moveHandler.js
@@ -2,7 +2,7 @@ import { Clutter, GLib, GObject, Gio, Meta, Mtk } from '../dependencies/gi.js';
 import { Main, WindowManager } from '../dependencies/shell.js';
 import { WINDOW_ANIMATION_TIME } from '../dependencies/unexported/windowManager.js';
 
-import { Orientation, MoveModes, Settings, is_wayland_compositor } from '../common.js';
+import { MoveModes, Orientation, Settings } from '../common.js';
 import { Rect, Util } from './utility.js';
 import { TilingWindowManager as Twm } from './tilingWindowManager.js';
 
@@ -319,7 +319,7 @@ export default class TilingMoveHandler {
         const altL = Clutter.ModifierType.MOD1_MASK;
         const altGr = Clutter.ModifierType.MOD5_MASK;
         const meta = Clutter.ModifierType.MOD4_MASK;
-        const rmb = is_wayland_compositor()
+        const rmb = Util.is_wayland_compositor()
             ? Clutter.ModifierType.BUTTON2_MASK
             : Clutter.ModifierType.BUTTON3_MASK;
         const pressed = [ // idxs come from settings

tiling-assistant@leleat-on-github/src/extension/tilingPopup.js
@@ -3,7 +3,7 @@ import { Main } from '../dependencies/shell.js';
 import * as SwitcherPopup49 from '../dependencies/unexported/switcherPopup.js';
 import * as SwitcherPopup48 from '../dependencies/unexported/switcherPopup-48.js';
 
-import { Direction, Orientation, is_wayland_compositor } from '../common.js';
+import { Direction, Orientation } from '../common.js';
 import { Util } from './utility.js';
 import { TilingWindowManager as Twm } from './tilingWindowManager.js';
 import * as AltTab from './altTab.js';
@@ -56,7 +56,7 @@ export const TilingSwitcherPopup = GObject.registerClass({
 
         // Destroy popup when touching outside of popup
         this.connect('touch-event', () => {
-            if (is_wayland_compositor())
+            if (Util.is_wayland_compositor())
                 this.fadeAndDestroy();
 
             return Clutter.EVENT_PROPAGATE;

tiling-assistant@leleat-on-github/src/extension/utility.js
@@ -1,4 +1,4 @@
-import { Clutter, Gio, GLib, Mtk, St } from '../dependencies/gi.js';
+import { Clutter, Gio, GLib, Meta, Mtk, St } from '../dependencies/gi.js';
 import { Config, Main } from '../dependencies/shell.js';
 
 import { Direction, Orientation, Settings } from '../common.js';
@@ -185,6 +185,10 @@ export class Util {
      */
     static getShellVersion() {
         return ShellVerison;
+    }
+
+    static is_wayland_compositor() {
+        return Meta.is_wayland_compositor === undefined || Meta.is_wayland_compositor();
     }
 
     /**